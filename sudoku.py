f2fe0e22f41d5ea38733f616a53e1a19a662ce1e
elliotjordanufl <127452188+elliotjordanufl@users.noreply.github.com>
2023-04-25 20:42:59 -0400

    Attempted to code methods in Board Class

--- sudoku.py
+++ sudoku.py
@@ -1,12 +1,9 @@
-import sys, pygame
-
 class Cell:
     def __int__(self, value, row, col, screen):
         self.value = value
         self.row = row
         self.col = col
         self.screen = screen
-        self.is_selected = False
 
 
     def set_cell_value(self, value):
@@ -17,32 +14,14 @@ class Cell:
         self.sketched_value = value
 
     def draw(self):
-        if self.value == 0:
-            draw_value = False
-        else:
-            draw_value = True
-
+        pass
 
 class Board:
     def __init__(self, width, height, screen, difficulty):
         self.width = width
         self.height = height
         self.screen = screen
-        self.difficulty = difficulty
-
-        # thought this should be called in updateBoard
-        # but updateBoard is phrased to update the underlying list instead of the board??
-        self.cells = []
-        i = 0
-        while i < width:
-            j = 0
-            new_col = []
-            while j < height:
-                new_cell = Cell(0, j, i, self.screen)
-                new_col.append(new_cell)
-                j += 1
-            self.cells.append(new_col)
-            i += 1
+        self. difficulty = difficulty
 
     def draw(self):
         pass
